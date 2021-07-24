c169a53a6bf78c90ca38dbeac285ff97b0fe2e0e
Elia Brown <brownelijah143@gmail.com>
2021-07-23 23:18:45 -0600

    idk

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,3 +5,39 @@ var currentDate = now.format("MM DD YYYY");
 
 // Set date 
 $("#currentDay").text("Today's Date: " + currentDate);
+
+var tasks = {};
+
+var saveBtn = $("#save");
+
+
+var text = $("#textarea")
+.text()
+.trim()
+
+
+//function to check the time of the event and compare to current time
+function checkTime(time, Id) { 
+    if (currentDate > time) { 
+        $(Id).addClass("past"); 
+    }
+    if (currentDate < time) { 
+        $(Id).addClass("future"); 
+    }
+    if (currentDate == time) { 
+        $(Id).addClass("present");
+    }
+
+}
+
+$(document).ready(function () {
+    // saveBtn click listener 
+    $(".saveBtn").on("click", function () {
+        // Get nearby values of the description in JQuery
+        var text = $(this).siblings(".description").val();
+        var time = $(this).parent().attr("id");
+
+        // Save text in local storage
+        localStorage.setItem(time, text);
+    })
+})
